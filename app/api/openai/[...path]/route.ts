a9aa12016535973b184be6b1d10cd3b1d3924ca0
Oiching-SuiLin <13425535469@163.com>
2023-10-19 08:38:40 +0800

    fix mark openai key after try

--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -122,19 +122,20 @@ async function requestOpenaiWithRetry(
     if (subpath === OpenaiPath.ListModelPath && response.status === 200) {
       const resJson = (await response.json()) as OpenAIListModelResponse;
       const availableModels = getModels(resJson);
-      // const res = await fetch(
-      //   `https://dev-api.porthub.app/namecards/openaikey/markkey/`,
-      //   {
-      //     headers: {
-      //       Authorization: `Token ${token}`,
-      //       "Content-Type": 'application/json',
-      //     },
-      //     body: JSON.stringify({
-      //       isSucceed: true,
-      //       openai_key: apiKey
-      //     }),
-      //   },
-      // );
+      const res = await fetch(
+        `https://dev-api.porthub.app/namecards/openaikey/markkey/`,
+        {
+          method: "post",
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            isSucceed: true,
+            openai_key: apiKey,
+          }),
+        },
+      );
       return NextResponse.json(availableModels, {
         status: response.status,
       });
@@ -143,19 +144,20 @@ async function requestOpenaiWithRetry(
     return response;
   } catch (e) {
     console.error("[OpenAI] ", e);
-    // const res = await fetch(
-    //   `https://dev-api.porthub.app/namecards/openaikey/markkey/`,
-    //   {
-    //     headers: {
-    //       Authorization: `Token ${token}`,
-    //       "Content-Type": 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //       isSucceed: false,
-    //       openai_key: apiKey
-    //     }),
-    //   },
-    // );
+    const res = await fetch(
+      `https://dev-api.porthub.app/namecards/openaikey/markkey/`,
+      {
+        method: "post",
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          isSucceed: false,
+          openai_key: apiKey,
+        }),
+      },
+    );
     return await requestOpenaiWithRetry(req, trytime + 1, subpath, token);
   }
 }
